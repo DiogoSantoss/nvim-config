956c99b5b5b8ce04e658e2952b4b192fcd3e0f57
Diogo Santos <diogosantoss360@gmail.com>
2022-06-18 01:37:49 +0100

    Added settings

---
+++ lua/settings/init.lua
@@ -0,0 +1,18 @@
+local set = vim.opt
+
+set.expandtab = true		 
+set.smarttab = true
+set.shiftwidth = 4 		
+set.tabstop = 4			
+
+set.hlsearch = true
+set.incsearch = true
+set.ignorecase = true
+set.smartcase = true
+
+set.splitbelow = true
+set.splitright = true
+set.wrap = false
+set.fileencoding = 'utf-8'
+
+set.relativenumber = true
