5d70e64c5939ffaf758ef4d603e33dc556fa8b97
Diogo Santos <diogosantoss360@gmail.com>
2022-06-19 13:23:52 +0100

    Added terminal navigation keybinds

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -3,5 +3,11 @@ local opts = { noremap = true, silent = true }
 
 vim.g.mapleader = " " -- leader key
 
---> nvim tree mappings <--
+-- nvim tree mappings
 map("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+
+-- terminal navigation
+map("n", "<C-h>", "<C-\\><C-N><C-w>h", opts)
+map("n", "<C-l>", "<C-\\><C-N><C-w>l", opts)
+map("n", "<C-j>", "<C-\\><C-N><C-w>j", opts)
+map("n", "<C-k>", "<C-\\><C-N><C-w>k", opts)
